ab0bf74633dbf6303099b2037fabba6c603122f5
wei.lw.liu <wei.lw.liu@mobiledrivetech.com>
2022-10-11 16:05:05 +0800

    第五次提交

--- app/src/main/java/com/learn/demo/memory/MainActivity.kt
+++ app/src/main/java/com/learn/demo/memory/MainActivity.kt
@@ -5,7 +5,14 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -26,9 +33,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pairedNumbers: TextView
 
 
-    private val boardSize: BoardSize = BoardSize.EASY
+    private var boardSize: BoardSize = BoardSize.EASY
 
-    private lateinit var memoryGame : MemoryGame
+    private lateinit var memoryGame: MemoryGame
     private lateinit var adapter: MemoryAdapter
 
 
@@ -40,7 +47,57 @@ class MainActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.game_content)
         moveNumbers = findViewById(R.id.moveNumbers)
         pairedNumbers = findViewById(R.id.pairedNumbers)
+        pairedNumbers.setTextColor(ContextCompat.getColor(this, R.color.color_none))
+        setupGame()
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.reset_menu -> {
+                if (memoryGame.hasWin()) {
+                    setupGame()
+                } else {
+                    showAlertDialog("Are you sure to start a new Game?", null, object : View.OnClickListener {
+                        override fun onClick(v: View?) {
+                            setupGame()
+                        }
+                    })
+                }
+                return true
+            }
+            R.id.set_size -> {
+                showSizeSetDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showSizeSetDialog() {
+        val view = LayoutInflater.from(this).inflate(R.layout.set_size_layout, null)
+        val radioGroup: RadioGroup = view.findViewById(R.id.radio_group)
+        when (boardSize) {
+            BoardSize.EASY -> radioGroup.check(R.id.size_easy)
+            BoardSize.MEDIUM -> radioGroup.check(R.id.size_medium)
+            BoardSize.HARD -> radioGroup.check(R.id.size_hard)
+        }
+        showAlertDialog("chose new game mode", view, View.OnClickListener {
+            boardSize = when (radioGroup.checkedRadioButtonId) {
+                R.id.size_easy -> BoardSize.EASY
+                R.id.size_hard -> BoardSize.MEDIUM
+                else -> BoardSize.HARD
+            }
+            setupGame()
+        })
+    }
+
+    private fun setupGame() {
         memoryGame = MemoryGame(boardSize)
         adapter = MemoryAdapter(this, boardSize, memoryGame.memoryCards, object : MemoryAdapter
         .OnClickedPosition {
@@ -53,6 +110,17 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    private fun showAlertDialog(title: String, view: View?, optionItemOnClickListener: View.OnClickListener) {
+        AlertDialog.Builder(this).setTitle(title)
+            .setView(view)
+            .setNegativeButton("cancel", null)
+            .setPositiveButton("ok") { _, _ ->
+                optionItemOnClickListener.onClick(null)
+            }
+            .show()
+
     }
 
     private fun updateMemoryCardsFlip(position: Int) {
@@ -79,7 +147,6 @@ class MainActivity : AppCompatActivity() {
         }
         moveNumbers.setText("Move: ${memoryGame.getMoves()}")
         adapter.notifyDataSetChanged()
-
 
     }
 }
